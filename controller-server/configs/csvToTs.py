8c7514ba817811ba230b681aa0b7446177aeeb79
MaxRem <maxzheng0302@gmail.com>
2025-03-02 20:42:11 +0800

    Combine prop and character pinmap

--- controller-server/configs/csvToTs.py
+++ controller-server/configs/csvToTs.py
@@ -1,4 +1,3 @@
-import csv
 import json
 import os
 
@@ -13,23 +12,24 @@ def csv_to_ts_json(csv_file, ts_file):
                 continue
             if ',' not in line:
                 current_dancer = line
-                dancers[current_dancer] = {"dancer": current_dancer}
+                dancers[current_dancer] = {"fps":30,"OFPARTS":{}, "LEDPARTS":{}, "LEDPARTS_MERGE":{}}
             elif line.endswith(','):
                 current_dancer = line.split(',')[0].strip()
-                dancers[current_dancer] = {"dancer": current_dancer}
+                dancers[current_dancer] = {"fps":30,"OFPARTS":{}, "LEDPARTS":{}, "LEDPARTS_MERGE":{}}
             else:
                 part_info = [x.strip() for x in line.split(',')]
                 if current_dancer and len(part_info) == 3:
                     part_name, part_id, part_length = part_info
-                    dancers[current_dancer][part_name] = {
-                        "id": part_id,
-                        "length": int(part_length)
+                    dancers[current_dancer]["LEDPARTS"][part_name] = {
+                        "id": int(part_id),
+                        "len": int(part_length)
                     }
 
     with open(ts_file, 'w', encoding='utf-8',newline=os.linesep) as f:
-        f.write("const dancers = ")
-        f.write(str(dancers).replace("'", '"'))
-        f.write(";\n\nexport default dancers;")
+        f.write("import { ModelPinMapTable} from \"@/schema/PinMapTable\";\n")
+        f.write("const PropPinMapTable: ModelPinMapTable = ")
+        json.dump(dancers, f, ensure_ascii=False, indent=2)
+        f.write(";\n\nexport default PropPinMapTable;")
 
-csv_to_ts_json('test.csv', 'test.ts')
+csv_to_ts_json('props_channel_software.csv', 'propPinMapTable.ts')
 print("Convert complete")
